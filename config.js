8fa39b359ab7d2a3fcd6aa906942128009e355d7
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-11-20 21:37:19 +0100

    Fixed env issue

--- config.js
+++ config.js
@@ -6,9 +6,6 @@ module.exports = {
   cloudninary_api_key: '178582552469755',
   cloudninary_api_secret: '_SRPd4Fo7fFqXBZ2IkuZz6cuaLo',
   decrypt_me: 'WHO_IS_KING_JIMMY',
-  db_user: 'zoqmvalxterqmh',
-  db_database: 'db8evnrv60k4k8',
-  db_password: '6c4961dcbd0beb466450458ef9ae0e88f701468c0e0426ffbb1a7c7754622f48',
-  db_port: process.env.DB_PORT || 5432,
+  dbConnectionString: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/teamwork_devcdb',
   port: 5000,
 };
--- controllers/dbconnect.js
+++ controllers/dbconnect.js
@@ -2,10 +2,7 @@ const { Pool } = require('pg');
 const config = require('../config');
 
 const pool = new Pool({
-  user: config.db_user,
-  database: config.db_database,
-  password: config.db_password,
-  port: config.db_port,
+  connectionString: config.dbConnectionString,
 });
 
 module.exports = pool;
