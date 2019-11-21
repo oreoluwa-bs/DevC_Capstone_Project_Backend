df388658a64a74fc7b4d610ffe5e5e8274a78af9
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-11-21 11:02:24 +0100

    Updated gif and article

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 module.exports = {
-  cloudninary_cloud_name: 'ocloudn',
-  cloudninary_api_key: '178582552469755',
-  cloudninary_api_secret: '_SRPd4Fo7fFqXBZ2IkuZz6cuaLo',
+  cloudninary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'ocloudn',
+  cloudninary_api_key: process.env.CLOUDINARY_API_KEY || '178582552469755',
+  cloudninary_api_secret: process.env.CLOUDINARY_API_SECRET || '_SRPd4Fo7fFqXBZ2IkuZz6cuaLo',
   decrypt_me: 'WHO_IS_KING_JIMMY',
   dbConnectionString: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/teamwork_devcdb',
   port: process.env.PORT || 5000,
